b366f63cb3f68667ae7bec61f38c9ec0958c1952
xodmf1215 <xodmf1215@users.noreply.github.com>
2022-11-15 17:56:11 +0900

    decorator

---
+++ HeadFirstDesignPattern/Decorator_Coffe_Order/CondimentDecorator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeadFirstDesignPattern.Decorator_Coffe_Order
+{
+    //Beverage 객체가 들어갈 곳에 들어갈 수 있음 : Beverage 확장
+    public abstract class CondimentDecorator : Beverage
+    {
+        Beverage beverage;
+        public abstract string getDescription();
+    }
+}
